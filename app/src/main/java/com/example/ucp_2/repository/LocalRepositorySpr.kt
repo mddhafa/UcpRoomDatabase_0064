6295ff9d3935590ef2df0cb63267f7739ea0ec40
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-18 16:24:25 +0700

    membuat local repository suplier

--- app/src/main/java/com/example/ucp_2/repository/LocalRepositorySpr.kt
+++ app/src/main/java/com/example/ucp_2/repository/LocalRepositorySpr.kt
@@ -1,5 +1,30 @@
 package com.example.ucp_2.repository
 
-class LocalRepositorySpr {
-    
+import com.example.ucp_2.data.dao.SuplierDao
+import com.example.ucp_2.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositorySpr (
+    private val suplierDao: SuplierDao
+
+) : RepositorySpr{
+    override suspend fun insertSpr(suplier: Suplier) {
+        suplierDao.insertSuplier(suplier)
+    }
+
+    override fun getAllSpr () : Flow<List<Suplier>> {
+        return suplierDao.getAllSuplier()
+    }
+
+    override fun getSpr (id: String) : Flow<Suplier> {
+        return suplierDao.getSuplier(id)
+    }
+
+    override suspend fun deleteSpr (suplier: Suplier){
+        suplierDao.deleteSuplier(suplier)
+    }
+
+    override suspend fun updateSpr (suplier: Suplier){
+        suplierDao.updateSuplier(suplier)
+    }
 }
