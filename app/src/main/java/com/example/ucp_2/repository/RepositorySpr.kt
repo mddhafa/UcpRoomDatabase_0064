415c3371dbdc121760fa7d47288d7f1a31ad6a9c
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-18 16:23:56 +0700

    membuat local repository suplier

--- app/src/main/java/com/example/ucp_2/repository/RepositorySpr.kt
+++ app/src/main/java/com/example/ucp_2/repository/RepositorySpr.kt
@@ -1,4 +1,16 @@
 package com.example.ucp_2.repository
 
+import com.example.ucp_2.data.entity.Suplier
+import kotlinx.coroutines.flow.Flow
+
 interface RepositorySpr {
+    suspend fun insertSpr(suplier: Suplier)
+
+    fun getAllSpr () : Flow<List<Suplier>>
+
+    fun getSpr (id: String) : Flow<Suplier>
+
+    suspend fun deleteSpr(suplier: Suplier)
+
+    suspend fun updateSpr (suplier: Suplier)
 }
