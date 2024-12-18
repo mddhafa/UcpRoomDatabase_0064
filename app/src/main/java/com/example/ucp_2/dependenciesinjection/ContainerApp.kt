f27e94ac6af264a4a5c5ce18fe6758d9fd905eb5
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-18 17:06:26 +0700

    membuat interface untuk menyediakan repository barang dan dan mengimplementasikannya di dalam containerapp

--- app/src/main/java/com/example/ucp_2/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp_2/dependenciesinjection/ContainerApp.kt
@@ -1,2 +1,23 @@
 package com.example.ucp_2.dependenciesinjection
 
+import android.content.Context
+import com.example.ucp_2.data.database.TokoDatabase
+import com.example.ucp_2.repository.LocalRepositoryBrg
+import com.example.ucp_2.repository.LocalRepositorySpr
+import com.example.ucp_2.repository.RepositoryBrg
+import com.example.ucp_2.repository.RepositorySpr
+
+interface InterfaceContainerApp {
+    val repositoryBrg: RepositoryBrg
+    val repositorySpr: RepositorySpr
+}
+
+class ContainerApp(private val context: Context): InterfaceContainerApp{
+    override val repositoryBrg: RepositoryBrg by lazy {
+        LocalRepositoryBrg(TokoDatabase.getTokoDatabase(context).BarangDao())
+    }
+
+    override val repositorySpr: RepositorySpr by lazy {
+        LocalRepositorySpr(TokoDatabase.getTokoDatabase(context).SuplierDao())
+    }
+}
