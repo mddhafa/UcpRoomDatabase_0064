3c6c8cf234b7f5b7d8527a2b2d361d25da072cc6
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-19 22:10:16 +0700

    membuat class HomeTokoViewModel yang berisi variabel repository barang dan supplier yg digunakan untuk mengakses data

--- app/src/main/java/com/example/ucp_2/ui/viewmodel/HomeTokoViewModel.kt
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/HomeTokoViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.ucp_2.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
 import com.example.ucp_2.data.entity.Barang
 import com.example.ucp_2.data.entity.Suplier
+import com.example.ucp_2.repository.RepositoryBrg
+import com.example.ucp_2.repository.RepositorySpr
+
+class HomeTokoViewModel(
+    private val repositoryBrg: RepositoryBrg,
+    private val repositorySpr: RepositorySpr
+) : ViewModel(){
+
+}
 
 data class HomeUiState(
     val listbrg: List<Barang> = listOf(),
