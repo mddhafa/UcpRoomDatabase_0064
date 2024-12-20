3b1d461f580dcabd5663b64b0d7c257528575bc0
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-20 16:21:14 +0700

    membuat fungsi barangevent untuk menyimpan input form ke dalam entity

--- app/src/main/java/com/example/ucp_2/ui/viewmodel/BarangViewModel.kt
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/BarangViewModel.kt
@@ -1,5 +1,17 @@
 package com.example.ucp_2.ui.viewmodel
 
+import com.example.ucp_2.data.entity.Barang
+
+
+fun BarangEvent.toBarangEntity(): Barang = Barang(
+    id_brg = id_brg,
+    nama_brg = nama_brg,
+    deskripsi = deskripsi,
+    harga = harga,
+    stok = stok,
+    namaSuplier = namaSuplier
+)
+
 data class BarangEvent(
     val id_brg: String,
     val nama_brg: String,
