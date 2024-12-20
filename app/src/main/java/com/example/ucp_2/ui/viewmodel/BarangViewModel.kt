2a091a673652227135d80abbeb22443b75a25a6a
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-20 16:17:48 +0700

    membuat data class dan variabel untuk menyimpan data input form

---
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/BarangViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.ucp_2.ui.viewmodel
+
+data class BarangEvent(
+    val id_brg: String,
+    val nama_brg: String,
+    val deskripsi: String,
+    val harga: String,
+    val stok: String,
+    val namaSuplier: String,
+    )
