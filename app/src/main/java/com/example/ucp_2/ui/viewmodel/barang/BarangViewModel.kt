79a5cb589e87d7709e624cb36fd6e9cd37249f93
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-22 20:04:25 +0700

    Mengupdate id dan nama dari barang

--- app/src/main/java/com/example/ucp_2/ui/viewmodel/barang/BarangViewModel.kt
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/barang/BarangViewModel.kt
@@ -18,15 +18,15 @@ class BarangViewModel (
 
     fun updateState(barangEvent: BarangEvent){
         uiState = uiState.copy(
-            barangEvent = barangEvent,
+            barangEvent = barangEvent
         )
     }
 
     private fun validateFields(): Boolean{
         val event= uiState.barangEvent
         val errorState = FormErrorState(
-            id_brg = if (event.id_brg.isNotEmpty()) null else "Id Barang Tidak Boleh Kosong",
-            nama_brg = if (event.nama_brg.isNotEmpty()) null else "Nama Barang Tidak Boleh Kosong",
+            idBrg = if (event.idBrg.isNotEmpty()) null else "Id Barang Tidak Boleh Kosong",
+            namaBrg = if (event.namaBrg.isNotEmpty()) null else "Nama Barang Tidak Boleh Kosong",
             deskripsi = if (event.deskripsi.isNotEmpty()) null else "Deskripsi Tidak Boleh Kosong",
             harga = if (event.harga.isNotEmpty()) null else "Harga Tidak Boleh Kosong",
             stok = if (event.stok.isNotEmpty()) null else "Stok Tidak Boleh Kosong",
@@ -72,23 +72,23 @@ data class BrgUiState(
     val snackBarMessage: String? = null
 )
 data class FormErrorState(
-    val id_brg: String? = null,
-    val nama_brg: String? = null,
+    val idBrg: String? = null,
+    val namaBrg: String? = null,
     val deskripsi: String? = null,
     val harga: String? = null,
     val stok: String? = null,
     val namaSuplier: String? = null
 ){
     fun isValid(): Boolean{
-        return id_brg == null && nama_brg == null && deskripsi == null &&
+        return idBrg == null && namaBrg == null && deskripsi == null &&
                 harga == null && stok == null && namaSuplier == null
     }
 
 }
 
 fun BarangEvent.toBarangEntity(): Barang = Barang(
-    id_brg = id_brg,
-    nama_brg = nama_brg,
+    idBrg = idBrg,
+    namaBrg = namaBrg,
     deskripsi = deskripsi,
     harga = harga,
     stok = stok,
@@ -96,8 +96,8 @@ fun BarangEvent.toBarangEntity(): Barang = Barang(
 )
 
 data class BarangEvent(
-    val id_brg: String ="",
-    val nama_brg: String ="",
+    val idBrg: String ="",
+    val namaBrg: String ="",
     val deskripsi: String="",
     val harga: String ="",
     val stok: String ="",
