24bd3cc688736ecc02ed2d466a792c67afeda7fe
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-23 04:28:50 +0700

    Membuat data class formerrorsprstate dan spruistate

--- app/src/main/java/com/example/ucp_2/ui/viewmodel/suplier/SuplierViewModel.kt
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/suplier/SuplierViewModel.kt
@@ -9,6 +9,25 @@ import com.example.ucp_2.data.entity.Suplier
 import com.example.ucp_2.repository.RepositorySpr
 import kotlinx.coroutines.launch
 
+
+data class SprUiState(
+    val suplierEvent: SuplierEvent = SuplierEvent(),
+    val isEntryValid: FormErrorSprState = FormErrorSprState(),
+    val snackBarMessage: String? = null
+)
+
+data class FormErrorSprState(
+    val idSpr: String? = null,
+    val namaSpr: String? = null,
+    val kontak: String? = null,
+    val alamat: String? = null
+){
+    fun isSprValid(): Boolean {
+        return idSpr == null && namaSpr == null &&
+                kontak == null && alamat == null
+    }
+}
+
 fun SuplierEvent.toSuplierEntity() : Suplier = Suplier(
     idSpr = idSpr,
     namaSpr = namaSpr,
