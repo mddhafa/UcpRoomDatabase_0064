f148332eb5802a49d88ecf91ca59012f1ad018c2
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-22 20:25:28 +0700

    Membuat class HomeBrgViewModel yang terhubng ke repositorybrg dan status lainnya agar operasi berjalan

--- app/src/main/java/com/example/ucp_2/ui/viewmodel/barang/HomeBrgViewModel.kt
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/barang/HomeBrgViewModel.kt
@@ -1,6 +1,50 @@
 package com.example.ucp_2.ui.viewmodel.barang
 
 import com.example.ucp_2.data.entity.Barang
+import com.example.ucp_2.repository.RepositoryBrg
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeBrgViewModel(
+    private val repositoryBrg: RepositoryBrg
+) : ViewModel() {
+    open val homebrgUiState: StateFlow<HomeBrgUiState> = repositoryBrg.getAllBrg()
+        .filterNotNull()
+        .map {
+            HomeBrgUiState(
+                listBrg = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeBrgUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeBrgUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeBrgUiState(
+                isLoading = true,
+            )
+        )
+}
 
 data class HomeBrgUiState(
     val listBrg: List<Barang> = listOf(),
