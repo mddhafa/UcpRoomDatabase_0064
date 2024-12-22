b4e3f3be02e75ed16d88fa11157da7bf50910a09
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-22 20:06:23 +0700

    Mengupdate id dan nama dari barang

--- app/src/main/java/com/example/ucp_2/ui/viewmodel/barang/DetailBrgViewModel.kt
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/barang/DetailBrgViewModel.kt
@@ -1,12 +1,73 @@
 package com.example.ucp_2.ui.viewmodel.barang
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp_2.data.entity.Barang
+import com.example.ucp_2.repository.RepositoryBrg
+import com.example.ucp_2.ui.navigation.AlamatNavigasi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
+class DetailBrgViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryBrg: RepositoryBrg,
+) : ViewModel(){
+    private val _id : String = checkNotNull(savedStateHandle[AlamatNavigasi.DestinasiDetailBrg.IDBRG])
 
-fun Barang.toDetailUiEvent () : BarangEvent{
+    val detailUiState: StateFlow<DetailBrgUiState> = repositoryBrg.getBrg(_id)
+        .filterNotNull()
+        .map {
+            DetailBrgUiState(
+                detailUiEvent = it.toDetailBrgUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailBrgUiState(isLoading = true))
+            delay(600)
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailBrgUiState(
+                isLoading = true,
+            ),
+        )
+
+    fun deleteBrg(){
+        detailUiState.value.detailUiEvent.toBarangEntity().let {
+            viewModelScope.launch {
+                repositoryBrg.deleteBrg(it)
+            }
+        }
+    }
+}
+
+data class DetailBrgUiState(
+    val detailUiEvent: BarangEvent = BarangEvent(),
+    val listBrg: List<Barang> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty : Boolean
+        get() = detailUiEvent == BarangEvent()
+
+    val isUiEventNotEmpty : Boolean
+        get() = detailUiEvent != BarangEvent()
+}
+
+fun Barang.toDetailBrgUiEvent () : BarangEvent{
     return BarangEvent(
-        id_brg = id_brg,
-        nama_brg = nama_brg,
+        idBrg = idBrg,
+        namaBrg = namaBrg,
         deskripsi = deskripsi,
         harga = harga,
         stok = stok,
