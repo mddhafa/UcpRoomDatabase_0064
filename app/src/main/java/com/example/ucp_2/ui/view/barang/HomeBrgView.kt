a5c6b1f4ef0837fcca46b2ad7c5149dc427ee517
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-23 23:33:26 +0700

    Membuat fungsi card untuk membuat cardbrg yang warnanya menyesuaikan stok

---
+++ app/src/main/java/com/example/ucp_2/ui/view/barang/HomeBrgView.kt
@@ -0,0 +1,132 @@
+package com.example.ucp_2.ui.view.barang
+
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp_2.ui.viewmodel.PenyediaViewModel
+import androidx.compose.runtime.Composable
+import com.example.ucp_2.data.entity.Barang
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.ucp_2.ui.customwidget.Navbar
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ucp_2.ui.viewmodel.barang.HomeBrgUiState
+import com.example.ucp_2.ui.viewmodel.barang.HomeBrgViewModel
+import kotlinx.coroutines.launch
+
+
+@Composable
+fun CardBrg(
+    brg: Barang,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Int) -> Unit
+) {
+    val backgroundColor = when (brg.stok) {
+        0 -> Color.Gray.copy(alpha = 0.1f)
+        in 1..10 -> Color.Red.copy(alpha = 0.2f)
+        else -> Color.Green.copy(alpha = 0.2f)
+    }
+    Card(
+        onClick = { onDetailClick(brg.idBrg) },
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = backgroundColor
+        ),
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.PlayArrow, contentDescription = "")
+                Spacer(modifier = modifier.padding(4.dp))
+                Text(
+                    text = "ID: ${brg.idBrg}",
+                    fontSize = 16.sp,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Person, contentDescription = null)
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = brg.namaBrg,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Create, contentDescription = null)
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = brg.deskripsi,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 20.sp
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Info, contentDescription = null)
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "Stok: ${brg.stok}",
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 16.sp
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.ShoppingCart, contentDescription = null)
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "Harga: Rp${brg.harga}",
+                    fontWeight = FontWeight.Medium
+                )
+            }
+        }
+    }
+}
