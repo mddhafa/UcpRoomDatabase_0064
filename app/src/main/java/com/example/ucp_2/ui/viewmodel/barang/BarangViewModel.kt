543547a02fbd560d7ae427fea0a819c6f3abe994
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-24 05:06:14 +0700

    menupdate BarangViewModel teutama pada bagian tipe data

--- app/src/main/java/com/example/ucp_2/ui/viewmodel/barang/BarangViewModel.kt
+++ app/src/main/java/com/example/ucp_2/ui/viewmodel/barang/BarangViewModel.kt
@@ -1,77 +1,77 @@
-package com.example.ucp_2.ui.viewmodel.barang
+package com.example.ucp_2.ui.viewmodel.suplier
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp_2.data.entity.Barang
 import com.example.ucp_2.repository.RepositoryBrg
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-
 
 class BarangViewModel (
     private val repositoryBrg: RepositoryBrg
-): ViewModel (){
+) : ViewModel (){
 
-    var uiState by mutableStateOf(BrgUiState())
+    var uiBrgState by mutableStateOf(BrgUiState())
 
-    fun updateState(barangEvent: BarangEvent){
-        uiState = uiState.copy(
-            barangEvent = barangEvent
+    fun updateBrgState(barangEvent: BarangEvent){
+        uiBrgState = uiBrgState.copy(
+            barangEvent = barangEvent,
         )
     }
 
     private fun validateFields(): Boolean{
-        val event= uiState.barangEvent
-        val errorState = FormErrorState(
-            idBrg = if (event.idBrg.isNotEmpty()) null else "Id Barang Tidak Boleh Kosong",
-            namaBrg = if (event.namaBrg.isNotEmpty()) null else "Nama Barang Tidak Boleh Kosong",
-            deskripsi = if (event.deskripsi.isNotEmpty()) null else "Deskripsi Tidak Boleh Kosong",
-            harga = if (event.harga.isNotEmpty()) null else "Harga Tidak Boleh Kosong",
-            stok = if (event.stok.isNotEmpty()) null else "Stok Tidak Boleh Kosong",
-            namaSuplier = if (event.namaSuplier.isNotEmpty()) null else "Nama Supplier Tidak Boleh Kosong",
+        val event = uiBrgState.barangEvent
+        val errorState = FormErrorBrgState(
+            idBrg = if (event.idBrg >= 0) null else "Id Barang tidak boleh kosong",
+            namaBrg = if (event.namaBrg.isNotEmpty()) null else "Id Barang tidak boleh kosong",
+            deskripsi = if (event.deskripsi.isNotEmpty()) null else "Id Barang tidak boleh kosong",
+            harga = if (event.harga.isNotEmpty()) null else "Id Barang tidak boleh kosong",
+            stok = if (event.stok >= 0 ) null else "Id Barang tidak boleh kosong",
+            namaSuplier = if (event.namaSuplier.isNotEmpty()) null else "Id Barang tidak boleh kosong",
         )
-        uiState = uiState.copy(isEntryValid = errorState)
-        return errorState.isValid()
+
+        uiBrgState = uiBrgState.copy(isEntryValid = errorState)
+        return errorState.isBrgValid()
     }
+    fun saveData() {
+        val currentEvent = uiBrgState.barangEvent
 
-    fun saveData(){
-        val currentEvent = uiState.barangEvent
-
-        if(validateFields()){
+        if (validateFields()) {
             viewModelScope.launch {
                 try {
                     repositoryBrg.insertBrg(currentEvent.toBarangEntity())
-                    uiState = uiState.copy(
-                        snackBarMessage = "Data berhasil diisi",
-                        barangEvent = BarangEvent(),
-                        isEntryValid = FormErrorState()
+                    uiBrgState = uiBrgState.copy(
+                        snackBarMessage = "Data berhasil disimpan",
+                        barangEvent = BarangEvent(), //Reset input form
+                        isEntryValid = FormErrorBrgState() // Reset error state
                     )
-                }catch (e: Exception){
-                    uiState = uiState.copy(
+                } catch (e: Exception) {
+                    uiBrgState = uiBrgState.copy(
                         snackBarMessage = "Data gagal disimpan"
                     )
                 }
             }
-        }else{
-            uiState = uiState.copy(
-                snackBarMessage = "Input tidak valid, Periksa ulang data kamu"
+        } else {
+            uiBrgState = uiBrgState.copy(
+                snackBarMessage = "Input tidak valid. Periksa kembali data anda."
             )
         }
     }
+    //Reset pesan Snackbar setelah ditampilkan
     fun resetSnackBarMessage(){
-        uiState = uiState.copy(snackBarMessage = null)
+        uiBrgState = uiBrgState.copy(snackBarMessage = null)
     }
-
 }
 
 data class BrgUiState(
     val barangEvent: BarangEvent = BarangEvent(),
-    val isEntryValid: FormErrorState = FormErrorState(),
+    val isEntryValid: FormErrorBrgState = FormErrorBrgState(),
     val snackBarMessage: String? = null
 )
-data class FormErrorState(
+
+data class FormErrorBrgState(
     val idBrg: String? = null,
     val namaBrg: String? = null,
     val deskripsi: String? = null,
@@ -79,12 +79,20 @@ data class FormErrorState(
     val stok: String? = null,
     val namaSuplier: String? = null
 ){
-    fun isValid(): Boolean{
+    fun isBrgValid(): Boolean{
         return idBrg == null && namaBrg == null && deskripsi == null &&
                 harga == null && stok == null && namaSuplier == null
     }
-
 }
+
+data class BarangEvent(
+    val idBrg: Int= 0,
+    val namaBrg : String= "",
+    val deskripsi : String= "",
+    val harga : String= "",
+    val stok : Int= 0,
+    val namaSuplier : String= ""
+)
 
 fun BarangEvent.toBarangEntity(): Barang = Barang(
     idBrg = idBrg,
@@ -93,13 +101,4 @@ fun BarangEvent.toBarangEntity(): Barang = Barang(
     harga = harga,
     stok = stok,
     namaSuplier = namaSuplier
-)
-
-data class BarangEvent(
-    val idBrg: String ="",
-    val namaBrg: String ="",
-    val deskripsi: String="",
-    val harga: String ="",
-    val stok: String ="",
-    val namaSuplier: String ="",
 )
