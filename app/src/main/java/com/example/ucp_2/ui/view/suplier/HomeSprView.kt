507cce5594933cf01297d614bdad61865f114edd
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-24 00:16:03 +0700

    membuat fungsi card suplier

---
+++ app/src/main/java/com/example/ucp_2/ui/view/suplier/HomeSprView.kt
@@ -0,0 +1,111 @@
+package com.example.ucp_2.ui.view.suplier
+
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp_2.ui.viewmodel.PenyediaViewModel
+import androidx.compose.runtime.Composable
+import com.example.ucp_2.data.entity.Barang
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.ucp_2.ui.customwidget.Navbar
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ucp_2.data.entity.Suplier
+import com.example.ucp_2.ui.view.barang.ListBarang
+import com.example.ucp_2.ui.viewmodel.barang.HomeBrgUiState
+import com.example.ucp_2.ui.viewmodel.barang.HomeBrgViewModel
+import com.example.ucp_2.ui.viewmodel.suplier.HomeSprUiState
+import com.example.ucp_2.ui.viewmodel.suplier.HomeSprViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun CardSpr(
+    spr: Suplier,
+    modifier : Modifier = Modifier,
+){
+    Card (
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column (
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.PlayArrow, contentDescription = "")
+                Spacer(modifier = modifier.padding(4.dp))
+                Text(
+                        text = "ID: ${spr.idSpr}",
+                fontSize = 16.sp,
+                color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                Spacer(modifier = modifier.padding(4.dp))
+                Text(
+                    text = spr.namaSpr,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Call, contentDescription = "")
+                Spacer(modifier = modifier.padding(4.dp))
+                Text(
+                    text = spr.kontak,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.LocationOn, contentDescription = "")
+                Spacer(modifier = modifier.padding(4.dp))
+                Text(
+                    text = spr.alamat,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        }
+    }
+}
